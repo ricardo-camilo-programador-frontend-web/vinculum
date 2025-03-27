7e5be4452641d285666fd80e5342db5224903c0a
Ricardo Camilo | Programador | React & Vue.js | Frontend <39412058+ricardo-camilo-programador-frontend-web@users.noreply.github.com>
2025-03-27 20:37:12 -0300

    feat(api-docs): add swagger documentation routes
    Add routes for serving Swagger API documentation JSON and UI. This enables developers to easily access and interact with the API documentation directly from the application.

--- start/routes.js
+++ start/routes.js
@@ -15,10 +15,39 @@
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
+const swaggerSpec = require('../config/swagger')
 
 // Pagina inicial
 Route.get('/', () => {
   return { greeting: 'Hello world in JSON' }
+})
+
+// Documentação Swagger
+Route.get('/api-docs.json', ({ response }) => {
+  response.send(swaggerSpec)
+})
+
+Route.get('/api-docs', ({ response }) => {
+  response.send(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui.css">
+    </head>
+    <body>
+      <div id="swagger-ui"></div>
+      <script src="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui-bundle.js"></script>
+      <script>
+        window.onload = () => {
+          SwaggerUIBundle({
+            url: '/api-docs.json',
+            dom_id: '#swagger-ui'
+          })
+        }
+      </script>
+    </body>
+    </html>
+  `)
 })
 
 // Produtos
